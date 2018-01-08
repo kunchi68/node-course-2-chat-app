0e9458b740695c76261e1ffef60c507e20ca22cc
quincy <kunchi68@gmail.com>
2018-01-09 01:38:17 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -18,13 +18,30 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'mike',
-        text: 'See you then',
-        createAt: 123123
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
     });
 
     socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
+        //console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+
+        //socket.broadcast.emit('newMessage', {
+        //    from: message.from,
+        //    text: message.text,
+        //    createAt: new Date().getTime()
+        //});
     });
 
     socket.on('disconnect', () => {
