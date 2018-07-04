55a9173c1259c5d593f0f7722043a25445705017
quincy <kunchi68@gmail.com>
2018-07-04 22:58:49 +0800

    Review the usage of moment

--- playground/time.js
+++ playground/time.js
@@ -8,8 +8,14 @@
 // date.add(1, 'year').subtract(9, 'months');
 // console.log(date.format('MMM Do, YYYY'));
 
-var soimTimestamp = moment().valueOf();
-console.log(soimTimestamp);
+// Assignment
+// 10:35 am
+// 6:01 am
+// var date = moment();
+// console.log(date.format('h:mm a'))
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
 
 var createdAt = 1234;
 var date = moment(createdAt);
