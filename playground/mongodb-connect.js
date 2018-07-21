4697b143d4397b404bd81f8d6b9bf8e80964d08d
quincy <kunchi68@gmail.com>
2018-07-21 23:51:54 +0800

    learn how to query database

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -15,7 +15,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    // db.collection('todos').insertOne({
+    // db.collection('Todos').insertOne({
     //         text: 'Something to do',
     //         completed: false
     // }, (err, result) => {
