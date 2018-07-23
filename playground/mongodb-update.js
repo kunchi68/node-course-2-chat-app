7c9fafa02567bd471d610136cddd268f38e973cc
quincy <kunchi68@gmail.com>
2018-07-23 14:48:36 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+var obj = new ObjectID();
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b5571b4c3acc6a84fe2a9e9')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b5574cbc3acc6a84fe2ab4d')
+    }, {
+        $set: {
+            name: 'Quincy'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    db.close();
+});
