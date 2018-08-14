33755ff990a834dcd9a42e94915f2084a0870a09
quincy <kunchi68@gmail.com>
2018-08-14 13:14:58 +0800

    Add todo list update and delete function in client side.

--- public/js/todo_main.js
+++ public/js/todo_main.js
@@ -28,9 +28,11 @@ jQuery('#link_logout').on('click', function (e) {
         }
     }).done(function() {
         div_main.load(form_file + " #form_login", load_login_form);
+        div_content.empty();
     }).fail(function(jqXHR, textStatus, errorThrown) {
         alert("Logout failed!!!");
         div_main.load(form_file + " #form_login", load_login_form);
+        div_content.empty();
     });
 });
 
@@ -53,7 +55,6 @@ jQuery('#link_login').on('click', function(e) {
 
     div_main.load(form_file + " #form_login", load_login_form);
 });
-
 
 function load_login_form() {
     var form = $('#form_login');
@@ -138,8 +139,72 @@ function load_register_form() {
         }).fail(function(jqXHR, textStatus, errorThrown) {
             alert("Register failed!!!");
         });
-    
     });
+}
+
+function add_todo_delete_event() {
+    $("#div_content > table > tr > td > #todo_delete").on('click', function(e) {
+        e.preventDefault();
+
+        var tdData = $(this).closest("tr").children();
+        var id = $(tdData[0]).text();
+        var isDelete = confirm("Do you want to delete this todo item?");
+        if (!isDelete) return;
+
+        $.ajax({
+            type: "DELETE",
+            url: `/todos/${id}`,
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            headers: {'x-auth': mytoken},
+            success: function(output, status, xhr) {
+                alert("Delete todo item successfully!");
+            },
+        }).done(function() {
+            load_todo_list();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Delete todo item failed!!!");
+            load_todo_list();
+        });
+    });
+    
+}
+
+function add_todo_update_event() {
+    $("#div_content > table > tr > td > #todo_update").on('click', function(e) {
+        e.preventDefault();
+
+        var tdData = $(this).closest("tr").children();
+        var id = $(tdData[0]).text();
+        var json = {};
+        json['text'] = $(tdData[1]).text();
+        json['completed'] = $(tdData[2]).find("input:checkbox").is(':checked');
+        //alert(JSON.stringify(json));
+
+        $.ajax({
+            type: "PATCH",
+            url: `/todos/${id}`,
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            headers: {'x-auth': mytoken},
+            data: JSON.stringify(json),
+            success: function(output, status, xhr) {
+                alert("Update todo item successfully!");
+            },
+            dataType: "json",
+            contentType: "application/json"
+        }).done(function() {
+            load_todo_list();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Update todo item failed!!!");
+            load_todo_list();
+        });
+    });
+    
 }
 
 function load_todo_list() {
@@ -152,7 +217,7 @@ function load_todo_list() {
         crossDomain: true,
         headers: {'x-auth': mytoken},
         success: function(output, status, xhr) {
-            alert("Get todo list successfully!");
+            //alert("Get todo list successfully!");
         }
     }).done(function(data) {
         var template = jQuery('#todo-item-template').html();
@@ -161,11 +226,12 @@ function load_todo_list() {
         table.append('<tr><th>Todo item</th><th>Completed</th><th>Completed time</th><th>Operations</th></tr>')
         $.each(data['todos'], function() {
             //alert(JSON.stringify(this));
-            var completedStr = 'No';
+            var completedStr = '';
             var formattedTime = '';
             if (this['completedAt']) formattedTime = moment(this['completedAt']).format('h:mm a');
-            if (this['completed']) completedStr = 'Yes';
+            if (this['completed']) completedStr = 'checked';
             var html = Mustache.render(template, {
+                id: this['_id'],
                 text: this['text'],
                 completed: completedStr,
                 completedAt: formattedTime
@@ -173,6 +239,8 @@ function load_todo_list() {
             table.append(html);
         });
         div_content.append(table);
+        add_todo_update_event();
+        add_todo_delete_event();
     }).fail(function(jqXHR, textStatus, errorThrown) {
         alert("Get todo list failed!!!");
     });
